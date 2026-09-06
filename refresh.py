082f11d5b4f854fa98968f70a24820b2883da343
zgzhangwo-max <zgzhangwo@163.com>
2026-09-06 11:04:16 +0800

    update refresh.py

--- refresh.py
+++ refresh.py
@@ -8,7 +8,7 @@ import traceback
 import urllib.parse
 import urllib.request
 
-BVID = "BV1sLbH6CEwY"
+BVID = "BV17Ubn6wEus"
 UA = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
       "(KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
 BASE_HDRS = {"User-Agent": UA,
